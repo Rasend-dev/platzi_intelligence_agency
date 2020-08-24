59d2338aebf77aea20641b7a17c717df14257840
Andrew Molina <rasend20@gmail.com>
2020-08-24 12:08:04 -0700

    fetchData.js changed to the cloud

--- cia/fetchData.js
+++ cia/fetchData.js
@@ -3,7 +3,7 @@ let = $accordion = document.getElementById("accordion");
 let ciaData = null;
 
 fetch(
-  "./cia.json"
+  "https://app.scrapinghub.com/api/v2/datasets/q9IYge1GBpz/download?format=json"
 )
   .then((response) => {
     return response.json();
